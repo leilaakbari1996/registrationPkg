ffe8e22f0128629daf85081614e262a4f0d92554
leila <87874568+leilaakbari1996@users.noreply.github.com>
2024-04-09 12:57:37 +0330

    Update 2024_04_07_064142_create_users_table.php
    Update 2024_04_07_064142_create_users_table.php

--- src/database/migrations/2024_04_07_064142_create_users_table.php
+++ src/database/migrations/2024_04_07_064142_create_users_table.php
@@ -11,13 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
+        Schema::table('users', function (Blueprint $table) {
             $table->string('PhoneNumber');
             $table->rememberToken();
-            $table->string('password');
-            $table->timestamp('email_verified_at')->nullable();
-            $table->timestamps();
         });
     }
 
@@ -26,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+    
     }
 };
