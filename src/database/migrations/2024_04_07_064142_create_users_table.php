8da18313bca7d6484b72ab9a6869fdf333fa01b0
leila <87874568+leilaakbari1996@users.noreply.github.com>
2024-04-09 13:04:44 +0330

    Update 2024_04_07_064142_create_users_table.php
    change migration users

--- src/database/migrations/2024_04_07_064142_create_users_table.php
+++ src/database/migrations/2024_04_07_064142_create_users_table.php
@@ -9,12 +9,22 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
+
     public function up(): void
     {
-        Schema::table('users', function (Blueprint $table) {
+         if(Schema::exists('users'))
+        {
+    
+            Schema::table('users', function (Blueprint $table) {
+                $table->string('PhoneNumber');
+            });
+        }else{
+            $table->id();
             $table->string('PhoneNumber');
-            $table->rememberToken();
-        });
+            $table->string('password');
+            $table->timestamp('email_verified_at')->nullable();
+            $table->timestamps();
+        }
     }
 
     /**
