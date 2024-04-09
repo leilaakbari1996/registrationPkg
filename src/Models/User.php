776ace15365d5e7e1312d4652e3d10e4c51c7468
leila <87874568+leilaakbari1996@users.noreply.github.com>
2024-04-09 15:12:45 +0330

    Update User.php

--- src/Models/User.php
+++ src/Models/User.php
@@ -18,6 +18,8 @@ class User extends Authenticatable
      *
      * @var array<int, string>
      */
+    protected $table = 'lex_users';
+    
     protected $fillable = [
         'leila_PhoneNumber',
         'password'
